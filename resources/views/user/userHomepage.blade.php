fa1b2038217269b9089b656cdb98acae68f8b9ce
Franjunior <megafrandio.oane.cics@ust.edu.ph>
2023-12-06 06:31:16 +0800

    new file:   app/Http/Controllers/MusicListController.php 	new file:   app/Models/MusicList.php 	new file:   resources/views/admin/admin-musicList.blade.php 	modified:   resources/views/navigation-menu.blade.php 	new file:   resources/views/user/musicList.blade.php 	modified:   resources/views/user/userHomepage.blade.php 	modified:   routes/web.php

--- resources/views/user/userHomepage.blade.php
+++ resources/views/user/userHomepage.blade.php
@@ -3,7 +3,8 @@
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Home Page') }}
         </h2>
+        <h1>Welcome {{ auth()->user()->name }}</h1>
 
-        <h1> Welcome User</h1>
     </x-slot>
+
 </x-app-layout>
