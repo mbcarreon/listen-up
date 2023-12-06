0012d5d88dfc08836fc89d4145986d90c99d6800
Franjunior <megafrandio.oane.cics@ust.edu.ph>
2023-12-06 18:24:22 +0800

    modified:   app/Http/Controllers/HomeController.php 	modified:   resources/views/profile/show.blade.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -31,35 +31,40 @@ class HomeController extends Controller {
 
    public function updateProfile(Request $request) {
       $user = Auth::user();
-
+  
       // Validate the incoming request data
       $request->validate([
-         'name' => 'nullable|string|max:255',
-         'bio' => 'nullable|string|max:255',
-         'location' => 'nullable|string|max:255',
-         'birthdate' => 'nullable|date',
-         'profile_image' => 'required|mimes:jpg,jpeg,png',
+          'name' => 'nullable|string|max:255',
+          'bio' => 'nullable|string|max:255',
+          'location' => 'nullable|string|max:255',
+          'birthdate' => 'nullable|date',
+          'profile_image' => 'nullable|mimes:jpg,jpeg,png', // Make the profile image optional
       ]);
-
-      $profile_image = $request->file('profile_image');
-
-        $name_gen = hexdec(uniqid());
-        $img_ext = strtolower($profile_image->getClientOriginalExtension());
-        $image_name = $name_gen . '.' . $img_ext;
-        $up_loc = 'image/profile/';
-        $last_img = $up_loc . $image_name;
-
-        $profile_image->move($up_loc, $image_name);
-
-      // Update the user's profile information
+  
+      // Check if a new profile image is provided
+      if ($request->hasFile('profile_image')) {
+          $profile_image = $request->file('profile_image');
+  
+          $name_gen = hexdec(uniqid());
+          $img_ext = strtolower($profile_image->getClientOriginalExtension());
+          $image_name = $name_gen . '.' . $img_ext;
+          $up_loc = 'image/profile/';
+          $last_img = $up_loc . $image_name;
+  
+          $profile_image->move($up_loc, $image_name);
+  
+          // Update the user's profile image
+          $user->update(['profile_image' => $last_img]);
+      }
+  
+      // Update the user's profile information (excluding profile image if not provided)
       $user->update([
-         'name' => $request->input('name'),
-         'bio' => $request->input('bio'),
-         'location' => $request->input('location'),
-         'birthdate' => $request->input('birthdate'),
-         'profile_image' => $last_img,
+          'name' => $request->input('name'),
+          'bio' => $request->input('bio'),
+          'location' => $request->input('location'),
+          'birthdate' => $request->input('birthdate'),
       ]);
-
+  
       return redirect()->back()->with('success', 'Profile updated successfully');
-   }
+  }
 }
