b4e671cf57b78acde37128fba3164071863e65e2
ClonedJ <103239500+ClonedJ@users.noreply.github.com>
2023-12-11 12:02:10 +0800

    Feat: Add 'View Individual Track' in viewLike

--- resources/views/user/viewLike.blade.php
+++ resources/views/user/viewLike.blade.php
@@ -3,6 +3,7 @@
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Profile') }}
         </h2>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
         @if(auth()->user()->profile_image)
         <img src="{{ asset(auth()->user()->profile_image) }}" alt="Profile Image"
             style="width: 70px; height: 70px; border-radius: 50%; object-fit: cover;">
@@ -17,6 +18,8 @@
         <hr style="border-color: white;">
         <ul id="likedSongsList" style="white-space: nowrap; overflow-x: auto;"></ul>
 
+        @include('modals.viewSong')
+
         <script type="text/javascript">
             function displayLikedSongs() {
                 fetch('/get-liked-songs')
@@ -30,10 +33,12 @@
                                 `<h3>Liked Songs</h3>
                                 <ul>
                                     ${Object.values(likedSongs).map(song => 
-                                        `<li style="display: inline-block; margin-right: 10px;"> 
-                                            <img src="http://coverartarchive.org/release/${song.releaseId}/front" alt="Default Profile Image" style="width: 150px; height: 150px; border-radius: 5px 5px 0 0; object-fit: cover;">
-                                            ${song.title} 
-                                        </li>`).join('')}
+                                        `<a href="#" onclick="showSongModal('${song.id}'); return false;">
+                                            <li style="display: inline-block; margin-right: 10px;"> 
+                                                <img src="http://coverartarchive.org/release/${song.releaseId}/front" alt="Default Profile Image" style="width: 150px; height: 150px; border-radius: 5px 5px 0 0; object-fit: cover;">
+                                                ${song.title} 
+                                            </li>
+                                        </a>`).join('')}
                                 </ul>`;
                         } else {
                             likedSongsList.innerHTML = '<p>No liked songs found</p>';
