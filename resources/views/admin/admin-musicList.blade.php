87faf05301037f646236debf40b9961ac7abbad3
Franjunior <megafrandio.oane.cics@ust.edu.ph>
2023-12-17 17:07:46 +0800

    modified:   resources/views/admin/admin-musicList.blade.php 	modified:   resources/views/admin/adminHomepage.blade.php 	modified:   resources/views/navigation-menu.blade.php

--- resources/views/admin/admin-musicList.blade.php
+++ resources/views/admin/admin-musicList.blade.php
@@ -1,10 +1,8 @@
 <x-app-layout>
     <x-slot name="header">
-        <h1 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Music List') }}
-        </h1>
 
-        <h3> Here is the list of your music, Admin</h3>
+        <p>Reported Contents</p>
+        <hr style="border-color: white;">
 
        
 
